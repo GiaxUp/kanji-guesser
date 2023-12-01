866c72991833fafd3484e1b0c266b88dbadbbf2c
Giacomo Della Peruta <giax.95@live.it>
2023-12-01 12:36:23 +0100

    cleared useless stuff, added navbar and routes

---
+++ src/components/TopNavbar.tsx
@@ -0,0 +1,21 @@
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function TopNavbar() {
+  return (
+    <Navbar bg="dark" data-bs-theme="dark">
+      <Container>
+        <Navbar.Brand href="home">Omega</Navbar.Brand>
+        <Nav className="me-auto">
+          <Nav.Link href="learn">Learn</Nav.Link>
+          <Nav.Link href="review">Review</Nav.Link>
+          <Nav.Link href="news">News</Nav.Link>
+        </Nav>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default TopNavbar;
